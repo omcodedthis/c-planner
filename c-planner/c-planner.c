8cb6283ea4a51b4258e375adfe13ab5bf50986e4
oM <119602009+omcodedthis@users.noreply.github.com>
2023-02-28 22:04:19 +0800

    C

--- c-planner/c-planner.c
+++ c-planner/c-planner.c
@@ -24,21 +24,26 @@ typedef struct
 individual_schedule;
 
 
-// global arrays & variables
-char *all_duty_types[7] = {"DUTY1", "DUTY2", "DUTY3", "DUTY4", "DUTY5", "DUTY6", "DUTY7"};
-int all_duty_hours[7] = {8, 16, 28, 31, 13, 4, 4};
+// global arrays & variables, the size is limited to 16 to ensure sufficient slots
+// for each duty type & its respective hours.
+char *all_duty_types[16];
+int all_duty_hours[16];
 
 
 // function prototyping
+void get_duties(void);
 void get_names(int strength, individual_schedule whole_schedule[]);
 void plan_schedule(int strength, individual_schedule whole_schedule[]);
 void print_schedule(int strength, individual_schedule whole_schedule[]);
 int random_int(int lower, int upper);
 
 
-
+// main() gets the duty types & its respective values from the user. The strength (total number of people) is assigned from
+// the user & their respective names. The schedule is then planned & printed to the terminal.
 int main(void)
 {
+
+    get_duties();
 
     int strength = get_int("Total People:");
 
@@ -53,7 +58,21 @@ int main(void)
 }
 
 
+// get_duties() asks the user for the total types of duties. The for-loop asks the user for the duty type and its respective
+// hours, repeating for the total of the types of duty.
+void get_duties(void)
+{
+    int total_duties = get_int("How many duties?:");
+    for (int i = 0; i < total_duties; i++)
+    {
+        all_duty_types[i] = get_string("Duty Name:");
+        all_duty_hours[i] = get_int("Total hours for this duty:");
+    }
+}
 
+
+
+// get_names() using the strength previously asked by the user, asks the user for each name part of this strength.
 void get_names(int strength, individual_schedule whole_schedule[])
 {
     for (int i = 0; i < strength; i++)
@@ -63,10 +82,12 @@ void get_names(int strength, individual_schedule whole_schedule[])
 }
 
 
+// plan_schedule() uses multiple for-loops to plan the schedule. i_length represents the total possible slots for each person.
 void plan_schedule(int strength, individual_schedule whole_schedule[])
 {
     int i_length = 16;
 
+// This for-loop assigns a default value to each slot in the array to prevent segmentation faults from occuring.
     for (int a = 0; a < strength; a++)
     {
         for(int b = 0; b < i_length; b++)
@@ -77,7 +98,9 @@ void plan_schedule(int strength, individual_schedule whole_schedule[])
     }
 
 
-    // 8pm to 9pm duty
+// This for-loop plans the duty schedule for the hours of 2000 to 2100hrs. The person decided for duty is chosen
+// randomly & given a random duty. The person's duty_type, is_duty is then assigned its respective value. The
+// total duty_hours for this respective duty is subtracted by one.
     for (int i = 0; i < 4; i++)
     {
         int random = random_int(0 , strength);
@@ -90,7 +113,9 @@ void plan_schedule(int strength, individual_schedule whole_schedule[])
     }
 
 
-    // 0700 to 0900 duty
+// This for-loop plans the duty schedule for the hours of 0700 to 0900hrs. The person decided for duty is chosen
+// randomly & given a random duty. The person's duty_type, is_duty is then assigned its respective value. The
+// total duty_hours for this respective duty is subtracted by one.
     for (int j = 0; j < 4; j++)
     {
         int random = random_int(0 , strength);
@@ -105,7 +130,11 @@ void plan_schedule(int strength, individual_schedule whole_schedule[])
 
     }
 
-    // 1700 to 1900 duty
+
+
+// This for-loop plans the duty schedule for the hours of 1700 to 1900hrs. The person decided for duty is chosen
+// randomly & given a random duty. The person's duty_type, is_duty is then assigned its respective value. The
+// total duty_hours for this respective duty is subtracted by one.
     for (int k = 0; k < 4; k++)
     {
         int random = random_int(0 , strength);
@@ -120,7 +149,13 @@ void plan_schedule(int strength, individual_schedule whole_schedule[])
 
     }
 
-    // main day duty
+
+
+// This nested for-loop plans the duty schedule for the remaining hours. The person decided for duty is chosen randomly &
+// given a random duty. The person's duty_type, is_duty is then assigned its respective value. Thetotal duty_hours for
+//  this respective duty is subtracted by one. To determine the appropriate amount of breaks, if the currenthour is greater
+// than 0900 with a probability of 50%, the current person will receive a break if they had a duty 4 hours ago or a
+// consecutive 2 hours of duty.
     for (int l = 0; l < strength; l++)
     {
         for (int p = 2; p < i_length; p++)
@@ -152,6 +187,9 @@ void plan_schedule(int strength, individual_schedule whole_schedule[])
 }
 
 
+
+// print_schedule() prints the schedule. This nested for-loop prints the name, and the respective schedule for this person
+// for the 24hr period.
 void print_schedule(int strength, individual_schedule whole_schedule[])
 {
     int i_length = 16;
@@ -177,6 +215,9 @@ void print_schedule(int strength, individual_schedule whole_schedule[])
     }
 }
 
+
+
+// random_int() takes in 2 numbers, max & min values. It then returns a random integer between these two values.
 int random_int(int lower, int upper)
 {
     int num;
